c6d9ed3c40b176d2bd0074f607c526eb836eacee
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-10-09 17:17:55 +0200

    Refactor Card + Nav

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeConsumer } from '../contexts/theme';
+import ThemeContext from '../contexts/theme';
 
-const Card = ({ header, image, subheader, link, text, children }) => (
-  <ThemeConsumer>
-    {theme => (
-      <div className={`card bg-${theme}`}>
-        <h4 className="header-lg center-text">{header}</h4>
-        <img className="avatar" src={image} alt={text} />
-        {subheader && <h4 className="center-text">{subheader}</h4>}
-        <h2 className="center-text">
-          <a className="link" href={link}>
-            {text}
-          </a>
-        </h2>
-        {children}
-      </div>
-    )}
-  </ThemeConsumer>
-);
+const Card = ({ header, image, subheader, link, text, children }) => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <div className={`card bg-${theme}`}>
+      <h4 className="header-lg center-text">{header}</h4>
+      <img className="avatar" src={image} alt={text} />
+      {subheader && <h4 className="center-text">{subheader}</h4>}
+      <h2 className="center-text">
+        <a className="link" href={link}>
+          {text}
+        </a>
+      </h2>
+      {children}
+    </div>
+  );
+};
 
 Card.propTypes = {
   header: PropTypes.string.isRequired,
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,49 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { ThemeConsumer } from '../contexts/theme';
+import ThemeContext from '../contexts/theme';
 
 const activeStyle = {
   color: 'rgb(187, 46, 31)',
 };
 
-const Nav = ({ toggleTheme }) => (
-  <ThemeConsumer>
-    {theme => (
-      <nav className="row space-between">
-        <ul className="row nav">
-          <li>
-            <NavLink
-              to="/"
-              exact
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Popular
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/battle"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Battle
-            </NavLink>
-          </li>
-        </ul>
-        <button
-          type="button"
-          style={{ fontSize: 30 }}
-          className="btn-clear"
-          onClick={toggleTheme}
-        >
-          {theme === 'light' ? '🔦' : '💡'}
-        </button>
-      </nav>
-    )}
-  </ThemeConsumer>
-);
+const Nav = ({ toggleTheme }) => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink to="/" exact activeStyle={activeStyle} className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/battle" activeStyle={activeStyle} className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        type="button"
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
+  );
+};
 
 Nav.propTypes = {
   toggleTheme: PropTypes.func.isRequired,
